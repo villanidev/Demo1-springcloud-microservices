10801d015df1fa1f80bc165d4e4b602e11bdc5d6
villanidev <villani.thiago01@gmail.com>
2022-01-14 23:14:04 -0300

    primeiro commit

---
+++ service-registry/src/test/java/com/villanidev/service/registry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,8 @@
+package com.villanidev.service.registry;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+}
---
+++ user-service/src/main/java/com/villanidev/user/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.villanidev.user.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.villanidev.user.entity.User;
+
+@Repository
+public interface UserRepository  extends JpaRepository<User,Long> {
+    User findByUserId(Long userId);
+}
